8b07c5b0932412880dd7038fc19519ed1bb9b13d
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2023-01-20 06:59:09 +0100

    import db

--- database/seeders/MoviesTableSeeder.php
+++ database/seeders/MoviesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Movie;
 
 class MoviesTableSeeder extends Seeder
 {
@@ -14,6 +15,18 @@ class MoviesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //legge i dati dal config
+        $movies = config('db');
+        //crea i film
+        foreach( $movies as $movie){
+            $new_movie = new Movie();
+            $new_movie->title = $movie['title'];
+            $new_movie->image = $movie['thumb'];
+            $new_movie->price = $movie['price'];
+            $new_movie->series = $movie['series'];
+            $new_movie->sale_date = $movie['sale_date'];
+            $new_movie->type = $movie['type'];
+            $new_movie->save();
+        }
     }
 }
